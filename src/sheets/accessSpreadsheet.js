a3bdc7f03106ffcb438c2a3d64d294431279e62f
Pedro Sena <pmfsl@cin.ufpe.br>
2020-04-13 19:26:01 -0300

    changing access

--- src/sheets/accessSpreadsheet.js
+++ src/sheets/accessSpreadsheet.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 const { SHEETS_URL, SHEETS_CREDENTIALS_CLIENT_EMAIL, SHEETS_CREDENTIALS_PRIVATE_KEY } = process.env
 
-const credencials = require('./credentials.json');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const StateData = require('./stateData');
 const WorldData = require('./worldData');
@@ -10,15 +9,12 @@ const CountryData = require('./countryData')
 
 async function AccessSpreadsheet() {
     //peganda a planilha usando a chave na URL
-    sheets_url = SHEETS_URL || credencials.sheets_url
-    const document = new GoogleSpreadsheet(sheets_url)
+    const document = new GoogleSpreadsheet(SHEETS_URL)
 
-    const email = SHEETS_CREDENTIALS_CLIENT_EMAIL || credencials.client_email
-    const key = SHEETS_CREDENTIALS_PRIVATE_KEY || credencials.private_key
     //Autenticação
     await document.useServiceAccountAuth({
-        client_email: email,
-        private_key: key,
+        client_email: SHEETS_CREDENTIALS_CLIENT_EMAIL,
+        private_key: SHEETS_CREDENTIALS_PRIVATE_KEY,
     });
 
     await document.loadInfo();
