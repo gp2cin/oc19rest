8f1f5edb5c9eb41a01f0a0ac194a835c3e850608
Pedro Sena <pmfsl@cin.ufpe.br>
2020-04-13 18:52:44 -0300

    movendo os arquivos e alterando as credenciais

--- src/sheets/accessSpreadsheet.js
+++ src/sheets/accessSpreadsheet.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const { SHEETS_URL, SHEETS_CREDENTIALS_CLIENT_EMAIL, SHEETS_CREDENTIALS_PRIVATE_KEY } = process.env
+
 const credencials = require('./credentials.json');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const StateData = require('./stateData');
@@ -6,11 +10,11 @@ const CountryData = require('./countryData')
 
 async function AccessSpreadsheet() {
     //peganda a planilha usando a chave na URL
-    sheets_id = process.env.SHEETS_ID || credencials.sheets_id
-    const document = new GoogleSpreadsheet(sheets_id)
+    sheets_url = SHEETS_URL || credencials.sheets_url
+    const document = new GoogleSpreadsheet(sheets_url)
 
-    const email = process.env.CLIENT_EMAIL || credencials.client_email
-    const key = process.env.PRIVATE_KEY || credencials.private_key
+    const email = SHEETS_CREDENTIALS_CLIENT_EMAIL || credencials.client_email
+    const key = SHEETS_CREDENTIALS_PRIVATE_KEY || credencials.private_key
     //Autenticação
     await document.useServiceAccountAuth({
         client_email: email,
