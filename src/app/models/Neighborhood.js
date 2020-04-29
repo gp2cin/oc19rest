da5cd2ed9fff10764fd37f91e2719a4e3267fc6c
Pedro Sena <pmfsl@cin.ufpe.br>
2020-04-29 18:47:42 -0300

    add neighborhood model

---
+++ src/app/models/Neighborhood.js
@@ -0,0 +1,33 @@
+const mongoose = require('../../services/database');
+
+const NeighborhoodSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    suspects: {
+        type: Number,
+        required: true,
+    },
+    confirmed: {
+        type: Number,
+        required: true,
+    },
+    deaths: {
+        type: Number,
+        required: true,
+    },
+    city: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'City'
+    },
+    updated_at: {
+        type: Date,
+        required: true,
+    },
+});
+
+module.exports = {
+    Neighborhood: mongoose.model('Neighborhood', NeighborhoodSchema),
+    NeighborhoodSchema,
+};
