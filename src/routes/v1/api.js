5f5edf2401fee910dcb7351b832c1aa4ef8d79b8
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-01 15:45:40 -0300

    edit file

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -7,13 +7,9 @@ const AuthController = require('../../app/controllers/AuthController');
 const PhoneController = require('../../app/controllers/PhoneController');
 const AddressController = require('../../app/controllers/AddressController');
 const WarningController = require('../../app/controllers/WarningController');
-<<<<<<< HEAD
 const ObserverReportController = require('../../app/controllers/ObserverReportController');
-const CaseController = require('../../app/controllers/CaseController');
-=======
 const OfficialCasesController = require('../../app/controllers/OfficialCasesController');
 const CrowdCasesController = require('../../app/controllers/CrowdCasesController')
->>>>>>> feature/neighborhood_model
 
 const updateGoogleSpreadsheet = require('../../googleSpreadsheets/updateSpreadsheet');
 
@@ -40,18 +36,13 @@ var APIRoutes = function (passport) {
   router.get('/warnings/map', WarningController.map);
   router.get('/warnings', WarningController.list);
 
-<<<<<<< HEAD
   router.post('/observer-report', ObserverReportController.store);
 
-  router.get('/cases', CaseController.list);
-  router.get('/cases/state', CaseController.find);
-=======
   router.get('/cases/official', OfficialCasesController.list);
   router.get('/cases/official/state', OfficialCasesController.find);
 
   router.get('/cases/crowd', CrowdCasesController.list);
-  //router.get('/cases/official/state', OfficialCasesController.find);
->>>>>>> feature/neighborhood_model
+  //router.get('/cases/crowd/city', OfficialCasesController.find);
 
   router.use('/docs', swaggerUi.serve);
   router.get('/docs', swaggerUi.setup(swaggerDocument));
