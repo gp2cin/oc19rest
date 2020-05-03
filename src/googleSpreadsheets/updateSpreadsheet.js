7a675f9a830528d05c67b8cbe1fcb2f7714a1127
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-03 17:27:14 -0300

    edit file

--- src/googleSpreadsheets/updateSpreadsheet.js
+++ src/googleSpreadsheets/updateSpreadsheet.js
@@ -11,63 +11,140 @@ const { State } = require('../app/models/State');
 const { Country } = require('../app/models/Country');
 const { CityOfficialCases } = require('../app/models/CityOfficialCases')
 
+async function updateWorldData(irrd, ocovid) {
+  let sheet = irrd.world;
+  let model = await World.find();
+
+  try {
+    if (!model) {
+      const record = await World.create(sheet);
+      await record.save();
+
+      sheet.updatedAt = moment(sheet.updatedAt).format('DD/MM/YYYY')
+      console.log(`Google Spreadsheet: World sheet updated at ${sheet.updatedAt}`);
+    } else {
+      let ocovid_sheet = ocovid.sheetsByIndex[0];
+      model = await World.findOne({ updatedAt: sheet.updatedAt })
+      if (!model) {
+        const record = await World.create(sheet);
+        await record.save();
+
+        sheet.updatedAt = moment(sheet.updatedAt).format('DD/MM/YYYY')
+        const row = await ocovid_sheet.addRow(sheet)
+        await row.save()
+
+        console.log(`Google Spreadsheet: World data updated at ${sheet.updatedAt}`);
+      }
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function updateCountryData(irrd, ocovid) {
+  let sheet = irrd.country;
+  let model = await Country.findOne();
+
+  try {
+    if (!model) {
+      const record = await Country.create(sheet);
+      await record.save();
+
+      sheet.updatedAt = moment(sheet.updatedAt).format('DD/MM/YYYY')
+
+      console.log(`Google Spreadsheet: Country data updated at ${sheet.updatedAt}`);
+    } else {
+      let ocovid_sheet = ocovid.sheetsByIndex[1];
+      model = await Country.findOne({ updatedAt: sheet.updatedAt })
+      if (!model) {
+        await Country.updateOne({ name: sheet.name }, sheet)
+
+        sheet.updatedAt = moment(sheet.updatedAt).format('DD/MM/YYYY')
+        const row = await ocovid_sheet.addRow(sheet)
+        await row.save()
+
+        console.log(`Google Spreadsheet: Country data updated at ${sheet.updatedAt}`);
+      }
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function updateStateData(irrd, ocovid) {
+  let sheet = irrd.state;
+  let { cities } = sheet
+  let model = await State.findOne();
+
+  try {
+    if (!model) {
+      const record = await State.create(sheet);
+      await record.save();
+
+      for (let c in cities) {
+        cities[c].state = record._id
+      }
+      await CityOfficialCases.insertMany(cities)
+
+      sheet.updatedAt = moment(sheet.updatedAt).format('DD/MM/YYYY')
+      console.log(`Google Spreadsheet: State data updated at ${sheet.updatedAt}`);
+    } else {
+      let ocovid_sheet = ocovid.sheetsByIndex[2];
+      model = await State.findOne({ updatedAt: sheet.updatedAt })
+      if (!model) {
+        await State.updateOne({ name: sheet.name }, sheet)
+        await writeCitiesData(cities, ocovid)
+
+        for (let c in cities) {
+          await CityOfficialCases.updateOne({ name: cities[c].name }, {
+            suspects: cities[c].suspects,
+            confirmed: cities[c].confirmed,
+            recovered: cities[c].recovered,
+            deaths: cities[c].deaths,
+            active: cities[c].active,
+            updatedAt: cities[c].updatedAt,
+          })
+        }
+
+        sheet.updatedAt = moment(sheet.updatedAt).format('DD/MM/YYYY')
+        console.log(`Google Spreadsheet: State data updated at ${sheet.updatedAt}`);
+
+        const row = await ocovid_sheet.addRow(sheet)
+        await row.save()
+      }
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function writeCitiesData(array, sheets) {
+  let cities = array.slice()
+  let sheet = sheets.sheetsByIndex[3];
+  try {
+    for (let c in cities) {
+      cities[c].updatedAt = moment(cities[c].updatedAt).format('DD/MM/YYYY')
+    }
+
+    await sheet.addRows(cities)
+
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 //atualiza a cada dia
 const update = cron.schedule(
   '0 * * * * *',
   async () => {
     const ocovid_doc = await AccessSpreadsheet(process.env.OCOVID19_SHEETS_URL)
     const irrd_doc = await AccessSpreadsheet(process.env.IRRD_SHEETS_URL);
-    const world_sheet = irrd_doc.world;
-    const country_sheet = irrd_doc.country;
-    const state_sheet = irrd_doc.state;
-
-    const world_model = await World.findOne({ updated_at: world_sheet.updated_at });
-    const country_model = await Country.findOne({ updated_at: country_sheet.updated_at });
-    const state_model = await State.findOne({ updated_at: state_sheet.updated_at });
 
     try {
-      if (!world_model) {
-        const record = await World.create(world_sheet);
-        await record.save();
+      await updateWorldData(irrd_doc, ocovid_doc)
+      await updateCountryData(irrd_doc, ocovid_doc)
+      await updateStateData(irrd_doc, ocovid_doc)
 
-        const ocovid_sheet = ocovid_doc.sheetsByIndex[0]
-        world_sheet.updated_at = moment(world_sheet.updated_at).format('DD/MM/YYYY')
-        await ocovid_sheet.addRow(world_sheet)
-
-        console.log(`Google Spreadsheet: World sheet updated at ${world_sheet.updated_at}`);
-      }
-      if (!country_model) {
-        const ocovid_sheet = ocovid_doc.sheetsByIndex[1]
-
-        await Country.updateOne({ name: country_sheet.name }, country_sheet);
-        country_sheet.updated_at = moment(country_sheet.updated_at).format('DD/MM/YYYY')
-        await ocovid_sheet.addRow(country_sheet)
-
-        console.log(`Google Spreadsheet: Country sheet updated at ${country_sheet.updated_at}`);
-      }
-      if (!state_model) {
-        const ocovid_sheet = ocovid_doc.sheetsByIndex[2]
-        const city_sheet = ocovid_doc.sheetsById[3]
-        const { cities } = state_sheet
-        const cities_array = cities
-
-        for (let c in cities) {
-          const { _id } = await State.findOne({ name: cities[c].state })
-          cities[c].state = _id
-          cities_array[c] = cities_array[c].updated_at = moment(cities_array[c].updated_at).format('DD/MM/YYYY')
-          await CityOfficialCases.updateOne({ name: cities[c].name }, cities[c])
-        }
-
-        await State.updateOne({ name: state_sheet.name }, state_sheet)
-        state_sheet.updated_at = moment(state_sheet.updated_at).format('DD/MM/YYYY')
-
-        await ocovid_sheet.addRow(state_sheet)
-        await city_sheet.addRows(cities_array)
-
-        console.log(`Google Spreadsheet: State sheet updated at ${state_sheet.updated_at}`);
-      } else {
-        console.log('Google Spreadsheet: Everything is updated');
-      }
     } catch (e) {
       console.log(e);
     }
