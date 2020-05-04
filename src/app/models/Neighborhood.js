1e08483ffc3099a20f25e689538fe77c788aabaa
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-04 17:59:41 -0300

    edit  neighborhood model

--- src/app/models/Neighborhood.js
+++ src/app/models/Neighborhood.js
@@ -1,32 +1,36 @@
 //Only crowd cases
 const mongoose = require('../../services/database');
+const { PolygonSchema } = require('./utils/PolygonSchema')
 
-const NeighborhoodSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require: true,
-    },
-    suspects: {
-        type: Number,
-        required: true,
-    },
-    confirmed: {
-        type: Number,
-        required: true,
-    },
-    deaths: {
-        type: Number,
-        required: true,
-    },
-    city: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'City'
-    },
-    updatedAt: {
-        type: Date,
-        required: true,
-    },
-});
+const NeighborhoodSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: true
+        },
+        suspects: {
+            type: Number,
+            required: true
+        },
+        confirmed: {
+            type: Number,
+            required: true
+        },
+        deaths: {
+            type: Number,
+            required: true
+        },
+        city: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'City'
+        },
+        updatedAt: {
+            type: Date,
+            required: true
+        },
+        location: PolygonSchema,
+    }
+);
 
 module.exports = {
     Neighborhood: mongoose.model('Neighborhood', NeighborhoodSchema),
