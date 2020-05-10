5e672bab711173743f96c73627d40257463854fb
raquel <sbsx77xavier@hotmail.com>
2020-05-10 13:35:01 -0300

    SignUp implemented

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -5,6 +5,7 @@ const { Individual } = require('../models/Individual');
 const { Address } = require('../models/Address');
 const { Phones } = require('../models/Phone');
 const authMiddleware = require('../middleware/auth');
+const moment = require('moment');
 
 class AuthController {
   /*
@@ -32,10 +33,12 @@ class AuthController {
    * Function to register and get token
    */
   async signUp(req, res) {
+    console.log(req.body)
     const { password, first_name, last_name, email, gender, birthdate } = req.body;
     try {
-      if (!(password || first_name || email))
+      if (!(password || first_name || email)) {
         res.status(404).send({ error: 'First name, email and password are required.' });
+      }
       const searchUser = await User.findOne({ email });
       if (searchUser) {
         res.status(400).send({ error: 'User alredy exists.' });
@@ -43,10 +46,8 @@ class AuthController {
         const user = await User.create({ password, first_name, last_name, email, active: true });
         if (user) {
           const individual = await Individual.create({
-            gender,
-            birthdate,
-            user: user._id,
-            active: true
+            gender: gender ? (['MALE', 'FEMALE'].includes(gender.toUpperCase()) ? gender.toUpperCase() : 'OTHER') : 'OTHER',
+            birthdate: moment(birthdate),
           });
           individual.createdAt = new Date();
 
