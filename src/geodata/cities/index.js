a88c064fa39f9ce98f82cada24d5178c5925aa00
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-11 14:40:38 -0300

    edit insert geojson files

--- src/geodata/cities/index.js
+++ src/geodata/cities/index.js
@@ -1,35 +1,37 @@
 const file = require('./cidades.json');
 const { features } = file
 const moment = require('moment');
-//const { City } = require('../../app/models/City');
+const { City } = require('../../app/models/City');
 const { State } = require('../../app/models/State');
 
 const formatName = require('../utils/formatName');
 const sortArrayByName = require('../utils/sortArrayByName');
 
 module.exports = async () => {
+    let city_record = await City.findOne()
     try {
-        let city_record = 0//await City.findOne()
         if (!city_record) {
             let sorted_array = sortArrayByName(features)
 
             let array_cities = [];
             const { _id } = await State.findOne({ name: 'pernambuco' })
             for (let f in sorted_array) {
-                const { type, geometry, properties } = sorted_array[f]
+                const { geometry, properties } = sorted_array[f]
                 array_cities.push({
-                    type: type,
-                    properties: {
-                        name_ca: formatName(properties.name),
-                        name: properties.description,
-                        state: _id,
-                    },
+                    name_ca: formatName(properties.name_ca),
+                    name: properties.name,
+                    state: _id,
                     geometry: geometry
                 });
             };
-            //await City.insertMany(array_cities);
-            //console.log(`Cities created at ${moment().format('DD/MM/YYYY')}`);
-            console.log(array_cities)
+
+            for (let c in array_cities) {
+                const record = await City.create(array_cities[c]);
+                record.save()
+            }
+
+            console.log(`Cities created at ${moment().format('DD/MM/YYYY')}`);
+            //console.log(array_cities[0])
         } else {
             console.log('ok')
         }
--- src/geodata/neighborhoods/index.js
+++ src/geodata/neighborhoods/index.js
@@ -8,25 +8,24 @@ const sortArrayByName = require('../utils/sortArrayByName');
 
 module.exports = async () => {
     try {
-        //const neighbor_record = await Neighborhood.findOne();
+        const neighbor_record = await Neighborhood.findOne();
         let sorted_array = sortArrayByName(features)
         let array_bairros = [];
 
-        //const { _id } = await City.findOne({ name_ca: 'recife' });
-        for (let f in sorted_array) {
-            const { type, geometry, properties } = sorted_array[f]
-            array_bairros.push({
-                type: type,
-                properties: {
+        if (!neighbor_record) {
+            //const { _id } = await City.findOne({ name_ca: 'recife' });
+            for (let f in sorted_array) {
+                const { type, geometry, properties } = sorted_array[f]
+                array_bairros.push({
                     name_ca: formatName(properties.name_ca),
                     name: properties.name,
-                },
-                geometry: geometry
-            });
-        };
-        //await Neighborhood.insertMany(array_bairros)
-        //console.log(`Recife neighborhoods created at ${moment().format('DD/MM/YYYY')}`);
-        console.log(array_bairros)
+                    city: 'recife',
+                    geometry: geometry
+                });
+            };
+            await Neighborhood.insertMany(array_bairros)
+            console.log(`Recife neighborhoods created at ${moment().format('DD/MM/YYYY')}`);
+        }
     } catch (error) {
         console.log(error)
     }
