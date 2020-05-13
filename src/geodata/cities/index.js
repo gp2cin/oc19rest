82af9ab169b610c21fd5725b23cded2f1fabfa8b
Pedro Sena <pmfsl@cin.ufpe.br>
2020-05-12 21:10:08 -0300

    edit goejson data

--- src/geodata/cities/index.js
+++ src/geodata/cities/index.js
@@ -10,17 +10,16 @@ module.exports = async () => {
         const city_docs = await City.find();
 
         if (city_docs.length == []) {
-            //const { _id } = await State.findOne({ name: 'pernambuco' })
+            const { _id } = await State.findOne({ name: 'pernambuco' })
             for (let f in features) {
                 const { geometry, properties } = features[f];
                 const record = await City.create({
                     name_ca: formatName(properties.name_ca),
                     name: properties.name,
-                    state: 'pernambuco',
+                    state: _id,
                     location: geometry
                 });
                 await record.save()
-                await City.createIndexes({ location: '2dsphere' })
             };
             console.log(`Cities created at ${moment().format('DD/MM/YYYY')}`);
         }
